0d7d3749d303852697787d1324afce92e2760b1d
Jack Robinson <jrobins@wharton.upenn.edu>
2012-09-05 23:30:03 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsexample::Application.config.secret_token = '3b9a0e9dd3a9176433f31051298bd64bfa9443a919e97815095f184873e1c5e958b97e479a21c4897ca890ac22cb9c28a6fc9824f6dd005968889fc887bbf632'
